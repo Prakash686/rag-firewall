3a3cdd2f56f6343d1c88d265b3f3933877a02893
Aishwarya Manoj <aishwaryamanojmc2gmail.com>
2026-05-10 19:14:35 +0530

    improved chunking

--- backend/app/services/chunker.py
+++ backend/app/services/chunker.py
@@ -1,21 +1,24 @@
-def chunk_documents(documents, chunk_size=100, overlap=20):
-    chunks = []
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def chunk_documents(documents,chunk_size=3,overlap=1):
+    chunks =[]
 
     for doc in documents:
         text = doc["content"]
         filename = doc["filename"]
 
+        parsed = nlp(text)
+        sentences = [sent.text.strip() for sent in parsed.sents]
         start = 0
-        while start < len(text):
+        while start < len(sentences):
             end = start + chunk_size
-
-            chunk_text = text[start:end]
-
+            chunk_sentences = sentences[start:end]
+            chunk_text = " ".join(chunk_sentences)
             chunks.append({
-                "text": chunk_text,
+                "text":chunk_text,
                 "source": filename
             })
-
             start += chunk_size - overlap
-
     return chunks
